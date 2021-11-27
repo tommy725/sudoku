5f86a34c0b30840c16a83176c55fe1f97108c229
Tommy <afteryou725@gmail.com>
2021-11-27 18:10:50 +0100

    Aggregation module for jacoco connected report

---
+++ View/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    /**
+     * Method which starts the program.
+     * @param args - input data
+     */
+    public static void main(String[] args) {
+        System.out.println("View");
+    }
+}
