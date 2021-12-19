e2287981ac9ac5630e39da6240881b6b4166f07a
Tommy <afteryou725@gmail.com>
2021-12-19 11:51:14 +0100

    MainFormController refactor

--- View/src/main/java/sudokuview/MainFormController.java
+++ View/src/main/java/sudokuview/MainFormController.java
@@ -1,8 +1,10 @@
 package sudokuview;
 
 import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
@@ -10,27 +12,19 @@ import javafx.stage.Stage;
 public class MainFormController {
     /**
      * Action after click the button.
+     *
      * @param actionEvent action which executed event
      */
     public void levelGenerate(ActionEvent actionEvent) {
-        Button button = (Button) actionEvent.getSource();
-        loadBoard(button.getId());
-    }
-
-    /**
-     * Method loads next scene.
-     * @param level level of the board
-     */
-    private void loadBoard(String level) {
         try {
+            Button button = (Button) actionEvent.getSource();
             FXMLLoader board = new FXMLLoader(
                     getClass().getResource("/Board.fxml")
             );
-            board.setController(new BoardController(level));
-            Stage stage = new Stage();
+            board.setController(new BoardController(button.getId()));
+            Stage stage = (Stage) (((Node) actionEvent.getSource()).getScene().getWindow());
             stage.setScene(new Scene(board.load()));
             stage.setTitle("TurboSudoku");
-            stage.show();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
